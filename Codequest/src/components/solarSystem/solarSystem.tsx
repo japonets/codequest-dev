0b259fe80e02639fce923730f8d72e56ef69e1f6
Camomila <camilacs1402@gmail.com>
2026-06-23 19:16:17 -0300

    feat: add theme toggle and background transition

--- Codequest/src/components/solarSystem/solarSystem.tsx
+++ Codequest/src/components/solarSystem/solarSystem.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { planets } from '../../data/planets';
-import { drawSun, drawPlanets, drawOrbits, drawStars } from '../../utils/drawHelpers';
+import { drawSun, drawPlanets, drawOrbits, drawStars, drawBackground } from '../../utils/drawHelpers';
 import useAnimation from '../../hooks/useAnimation';
 import type { Star, ShinyStar } from "../../data/stars";
 import type { PlanetPosition } from "../../data/planets";
@@ -8,14 +8,18 @@ import sol from "../../assets/sol.png"
 
 const SolarSystem: React.FC = () => {
 
+    const [tema, setTema] = useState<'dark' | 'light'>('dark'); //useState é um hook do React que permite criar um estado, nesse caso, para o tema, que pode ser "dark" ou "light". O tipo <"dark" | "light"> é usado para garantir que o estado só possa ter esses dois valores.
+
+    
     const canvasRef = React.useRef<HTMLCanvasElement>(null); //useRef é um hook do React que permite criar uma referência a um elemento do DOM, nesse caso, o canvas. O tipo HTMLCanvasElement é usado para garantir que a referência seja do tipo correto.
-
+    
     const planetAngles = useRef(
-
+        
         planets.map((_, index) => index * 1.5) //inicializa os ângulos de cada planeta com um valor diferente para que eles não fiquem todos alinhados no início da animação
-
-        ); //useRef é usado para armazenar os ângulos de cada planeta, que serão atualizados a cada frame para criar a animação de rotação. 
-
+        
+    ); //useRef é usado para armazenar os ângulos de cada planeta, que serão atualizados a cada frame para criar a animação de rotação. 
+    
+    const transitionRef = useRef(0);
     const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
     const centerRef = useRef({ x: 0, y: 0 });
     const planetPositionRef = useRef<PlanetPosition[]>([]);
@@ -26,10 +30,9 @@ const SolarSystem: React.FC = () => {
     useEffect(() => {
         const canvas = canvasRef.current;
         if (!canvas) return;
-
         
         const handleResize = () => { 
-
+            
             canvas.width = window.innerWidth
             canvas.height = window.innerHeight
             centerRef.current.x = canvas.width / 2;
@@ -41,7 +44,7 @@ const SolarSystem: React.FC = () => {
                 radius: Math.random() * 2,
                 opacity: Math.random(),
             }))
-        
+            
             shinyStarsRef.current = Array.from({ length: 100 }, () => ({
                 x: Math.random() * canvas.width,
                 y: Math.random() * canvas.height,
@@ -51,16 +54,19 @@ const SolarSystem: React.FC = () => {
             }));
             
         }//função para lidar com o redimensionamento da janela, garantindo que o canvas se ajuste ao novo tamanho
-
+        
+        
+        
         const centerX = centerRef.current.x = canvas.width / 2;
         const centerY = centerRef.current.y = canvas.height / 2;
-
+        
         
         planetPositionRef.current = planets.map((planet, index) => ({
             x: centerX + planet.orbitRadius * Math.cos(planetAngles.current[index]),
             y: centerY + planet.orbitRadius * Math.sin(planetAngles.current[index]),
             size: planet.size,
             color: planet.color,
+            lightColor: planet.lightColor,
             orbitRadius: planet.orbitRadius,
         }));
         
@@ -72,7 +78,7 @@ const SolarSystem: React.FC = () => {
         const img = new Image()
         img.src = sol
         img.onload = () => { sunImageRef.current = img }
-
+        
         starsRef.current = Array.from({ length: 300 }, () => ({
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
@@ -87,52 +93,62 @@ const SolarSystem: React.FC = () => {
             opacity: Math.random(),
             shine: 0.01 + Math.random() * 0.01, //velocidade de brilho aleatória para cada estrela brilhante, criando um efeito mais natural de piscar
         }));
-
+        
         return () => window.removeEventListener('resize', handleResize)
-
+        
     }, []); //o array vazio [] indica que o efeito deve ser executado apenas uma vez, quando o componente for montado.
     
-    
     useAnimation(() => {
-      const canvas = canvasRef.current;
-      const ctx = ctxRef.current;
-      const centerX = centerRef.current.x;
-      const centerY = centerRef.current.y;
-      
-      if (!canvas || !ctx) return;
+        const canvas = canvasRef.current;
+        const ctx = ctxRef.current;
+        const centerX = centerRef.current.x;
+        const centerY = centerRef.current.y;
 
-      const radius = 30;
+        if(tema === 'light' && transitionRef.current < 1) {
+            transitionRef.current += 0.02;
+        }
+        
+        if(tema === 'dark' && transitionRef.current > 0) {
+            transitionRef.current -= 0.02;
+        }
 
-      ctx.clearRect(0, 0, canvas.width, canvas.height); //limpa o canvas para redesenhar os planetas na nova posição
+        if (!canvas || !ctx) return;
+        
+        const radius = 30;
 
-      //background
-      const gradient = ctx.createRadialGradient(centerX, centerY, 0, centerX, centerY, canvas.width * 0.75); //cria um gradiente radial para o fundo do canvas, com o centro no sol e se expandindo até as bordas do canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height); //limpa o canvas para redesenhar os planetas na nova posição
 
-      gradient.addColorStop(0,   '#220144')  // roxo escuro no centro
-      gradient.addColorStop(0.2, '#190331')  // azul escuro
-      gradient.addColorStop(0.5, '#0e0730')  // roxo/azul médio
-      gradient.addColorStop(0.75, '#0f051b')  // quase preto
-      gradient.addColorStop(1,   '#000000')  // preto nas bordas
-      ctx.fillStyle = gradient; //define o estilo de preenchimento como o gradiente criado
-      ctx.fillRect(0, 0, canvas.width, canvas.height); //preenche todo o canvas com o gradiente
+        //background
+        drawBackground(ctx, canvas.width, canvas.height, transitionRef.current, centerX, centerY);
 
-      //estrelas
-      drawStars(ctx, starsRef.current, shinyStarsRef.current); 
+        //estrelas
+        drawStars(ctx, starsRef.current, shinyStarsRef.current, tema); 
 
-      //sol
-      drawSun(ctx, centerX, centerY, radius, sunImageRef.current!); 
+        //sol
+        drawSun(ctx, centerX, centerY, radius, sunImageRef.current!); 
 
-      //orbitas
-      drawOrbits(ctx, planets, centerX, centerY);
+        //orbitas
+        drawOrbits(ctx, planets, centerX, centerY, tema);
 
-      planetAngles.current = planetAngles.current.map((angle, index) => angle + planets[index].rotationSpeed); //atualiza os ângulos de cada planeta com base na velocidade de rotação definida em planets
+        planetAngles.current = planetAngles.current.map((angle, index) => angle + planets[index].rotationSpeed); //atualiza os ângulos de cada planeta com base na velocidade de rotação definida em planets
 
-      drawPlanets(ctx, planetPositionRef.current, planetAngles.current, centerX, centerY); //desenha os planetas na nova posição calculada
+        drawPlanets(ctx, planetPositionRef.current, planetAngles.current, centerX, centerY, tema); //desenha os planetas na nova posição calculada
+
     });
+
 
   return (
     <div>
-        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} />
+        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}/>
+        <label className="mode-toggle">
+            <span className="icon">
+               <img src={tema === 'light' ? 'src/assets/light-mode.png' : 'src/assets/dark-mode.png'} alt={tema === 'light' ? "Light Mode" : "Dark Mode"} />
+            </span>
+            <label className="switch">
+                <input type="checkbox" onChange={() => setTema(tema === 'light' ? 'dark' : 'light')} />
+                <span className="slider round"></span>
+            </label>
+        </label>
     </div>
   );
 };
